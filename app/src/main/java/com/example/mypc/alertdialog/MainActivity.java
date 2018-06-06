02be2caf6d76bda09f5625faa3f820ebeeacab7a
AvinashKaur <avinashkaur520@gmail.com>
2018-06-06 20:40:43 +0530

    Alert Dialog Uses

--- app/src/main/java/com/example/mypc/alertdialog/MainActivity.java
+++ app/src/main/java/com/example/mypc/alertdialog/MainActivity.java
@@ -8,8 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity
+{
     Button btn;
     AlertDialog.Builder ab;
 
